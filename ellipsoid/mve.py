0c907bd70396cecf76fb9dee1e418c9c726906ec
Philipp Dufter <philipp.dufter@web.de>
2016-11-02 10:38:31 +0100

    some formatting

--- ellipsoid/mve.py
+++ ellipsoid/mve.py
@@ -7,13 +7,14 @@ from scipy.stats import chi2
 
 
 class mve(object):
+
     """
     Estimates the Minimum Volume Ellipsoid.
 
     For reading see: 
     http://onlinelibrary.wiley.com/doi/10.1002/env.628/abstract
     http://scikit-learn.org/stable/modules/generated/sklearn.covariance.MinCovDet.html
-    
+
     Parameters
     -----------
     n_samples: non-negative integer
@@ -27,7 +28,7 @@ class mve(object):
 
     random_state: non-negative integer
         Random state or seed
-    
+
     Attributes
     -----------
     n_data: non-negative integer
@@ -44,6 +45,8 @@ class mve(object):
         Center of the final MVE.
     vcov_hat: numpy nd array
         Covariance matrix of finale MVE.
+    X: numpy nd array
+        stored data
 
     References
     -----------
@@ -54,10 +57,10 @@ class mve(object):
 
     def __init__(
         self,
-        n_samples = 10000,
-        required_n_data = None
-        singularity_add_samples = 1, 
-        random_state = None
+        n_samples=10000,
+        required_n_data=None,
+        singularity_add_samples=1,
+        random_state=None
     ):
 
         self.n_samples = n_samples
@@ -70,16 +73,15 @@ class mve(object):
 
     def get_params(self):
         """Return parameters as a dictionary.
-        # TODO potentially later inherit from EmpiricalCovariance in SciKit Learn
 
         Returns
         -------
         Dictionary of parameters
         """
         params = {
-            'n_samples': self.n_samples, 
+            'n_samples': self.n_samples,
             'required_n_data': self.required_n_data,
-            'singularity_add_samples': self.singularity_add_samples, 
+            'singularity_add_samples': self.singularity_add_samples,
             'random_state': self.random_state
         }
         return params
@@ -97,7 +99,6 @@ class mve(object):
         for parameter, value in parameters.items():
             setattr(self, parameter, value)
         return self
-
 
     def fit(self, X):
         """Fits a minimum volume ellipsoid given data X
@@ -120,7 +121,8 @@ class mve(object):
 
         tolerance = 0.1
         if numpy.abs(numpy.linalg.det(numpy.cov(X.transpose()))) <= tolerance:
-            raise ValueError("Lack of variance in data.", numpy.abs(numpy.linalg.det(numpy.cov(X))))
+            raise ValueError(
+                "Lack of variance in data.", numpy.abs(numpy.linalg.det(numpy.cov(X))))
 
         self.n_data, self.n_features = X.shape
 
@@ -132,7 +134,8 @@ class mve(object):
 
         for i in range(0, self.n_samples):
             # potentially allow for deterministic order as well
-            sample_indices = numpy.random.choice(range(0, self.n_data), size=self.n_features+1, replace=False)
+            sample_indices = numpy.random.choice(
+                range(0, self.n_data), size=self.n_features+1, replace=False)
             sample_data = X[sample_indices].transpose()
 
             mean = sample_data.mean(axis=1)
@@ -141,10 +144,12 @@ class mve(object):
             max_iter_singularity = self.required_n_data
             j = 0
             while numpy.linalg.det(vcov) == 0 and j <= max_iter_singularity:
-                # prevent duplicated indices 
-                remaining_indices = numpy.setdiff1d(range(0, self.n_data), sample_indices)
-                add_sample_index = numpy.random.choice(remaining_indices, size=self.singularity_add_samples)
-                # prevent duplicated indices 
+                # prevent duplicated indices
+                remaining_indices = numpy.setdiff1d(
+                    range(0, self.n_data), sample_indices)
+                add_sample_index = numpy.random.choice(
+                    remaining_indices, size=self.singularity_add_samples)
+                # prevent duplicated indices
 
                 sample_indices = numpy.append(sample_indices, add_sample_index)
 
@@ -156,24 +161,30 @@ class mve(object):
             if numpy.linalg.det(vcov) == 0:
                 raise ValueError("Singular Data")
 
-            # either for loop or a lot of redundant caluclations (but vectorised)
-            X_minus_mean = X - numpy.tile(mean, (self.n_data,1))      
-            m_J_squared_array = numpy.diag(X_minus_mean.dot(numpy.linalg.inv(vcov)).dot(X_minus_mean.transpose()))
+            # either for loop or a lot of redundant caluclations (but
+            # vectorised)
+            X_minus_mean = X - numpy.tile(mean, (self.n_data, 1))
+            m_J_squared_array = numpy.diag(
+                X_minus_mean.dot(numpy.linalg.inv(vcov)).dot(X_minus_mean.transpose()))
             m_J_squared = numpy.sort(m_J_squared_array)[self.required_n_data]
 
-            P_J_tmp = numpy.sqrt(m_J_squared ** self.n_features * numpy.linalg.det(vcov))
+            P_J_tmp = numpy.sqrt(
+                m_J_squared ** self.n_features * numpy.linalg.det(vcov))
             if self.P_J > P_J_tmp:
                 self.resulting_indices = sample_indices.copy()
                 self.resulting_data = X[sample_indices].transpose()
                 self.P_J = P_J_tmp
 
-        sample_correction_term = (1 + 15 / (self.n_data - self.n_features)) **2
+        sample_correction_term = (
+            1 + 15 / (self.n_data - self.n_features)) ** 2
 
         chi2_med = chi2.median(self.n_features, loc=0, scale=1)
-        
+
         T_X = self.resulting_data.mean(axis=1)
-        C_X = sample_correction_term * (1 / chi2_med) * m_J_squared * numpy.cov(self.resulting_data)
-        
+        C_X = sample_correction_term * \
+            (1 / chi2_med) * m_J_squared * numpy.cov(self.resulting_data)
+
+        self.X = X.copy()
         self.mean_hat = T_X
         self.vcov_hat = C_X
 
@@ -185,7 +196,7 @@ class mve(object):
         Parameters
         -----------
         X: numpy nd array (axis 0: datapoints, axis 1: features)
-        
+
         Returns
         --------
         numpy nd array in length of number of datapoints
@@ -193,12 +204,11 @@ class mve(object):
 
         if X is None:
             X = self.X
-        
-        X_minus_mean = X - numpy.tile(self.mean_hat, (len(X), 1))    
+
+        X_minus_mean = X - numpy.tile(self.mean_hat, (len(X), 1))
 
         return numpy.diag(X_minus_mean.dot(numpy.linalg.inv(self.vcov_hat)).dot(X_minus_mean.transpose()))
 
 
 if __name__ == "__main__":
     pass
-
